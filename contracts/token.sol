6a0a2adfeaf218249cac26959d82d0a91f723051
TyreseLian <tyreselian3915@gmail.com>
2017-10-12 15:49:55 -0600

    adjust token, setup genesis block

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.14;
 
-contract TokenRecipient {
+contract TobaRecipient {
    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
 }
 
-contract MyToken {
+contract TobaToken {
    string public name;
    string public symbol;
    uint8 public decimals;
@@ -17,7 +17,7 @@ contract MyToken {
    event Transfer(address indexed from, address indexed to, uint256 value);
 
    /* Initializes contract with initial supply tokens to the creator of the contract */
-   function MyToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
+   function TobaToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
       balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
       name = tokenName;                                   // Set the name for display purposes
       symbol = tokenSymbol;                               // Set the symbol for display purposes
@@ -37,7 +37,7 @@ contract MyToken {
 
    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
       allowance[msg.sender][_spender] = _value;
-      TokenRecipient spender = TokenRecipient(_spender);
+      TobaRecipient spender = TobaRecipient(_spender);
       spender.receiveApproval(msg.sender, _value, this, _extraData);
    }
 
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	// Create an IPC based RPC connection to a remote node
+	conn, err := ethclient.Dial("/home/karalabe/.ethereum/testnet/geth.ipc")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	// Instantiate the contract and display its name
+	token, err := NewToken(common.HexToAddress("0x21e6fc92f93c8a1bb41e2be64b4e1f88a54d3576"), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Token contract: %v", err)
+	}
+	name, err := token.Name(nil)
+	if err != nil {
+		log.Fatalf("Failed to retrieve token name: %v", err)
+	}
+	fmt.Println("Token name:", name)
+}
